9b5df143bab01cc655c7a06c4e1ada3403d8e311
TheDim0n <>
2023-05-08 11:22:20 +0300

    fix migrations; add Swagger to static

--- app/main.py
+++ app/main.py
@@ -1,15 +1,28 @@
+import time
+
 import importlib
 
 from importlib import resources
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.openapi.docs import (
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html,
+)
+from fastapi.staticfiles import StaticFiles
 
 from app.shared.deps import get_settings
 
 
 SETTINGS = get_settings()
 
-app = FastAPI(root_path=SETTINGS.root_path, title="Messages API")
+app = FastAPI(
+    title="Messages API",
+    root_path=SETTINGS.root_path,
+    docs_url=None,
+    redoc_url=None
+)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # include all routers
 plugins = [f[:-3] for f in resources.contents("app.routers")
@@ -28,3 +41,28 @@ if SETTINGS.debug:
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = f"{process_time * 1000} ms"
+    return response
+
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=f"{SETTINGS.root_path}{app.openapi_url}",
+        title=app.title,
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url=f"{SETTINGS.root_path}/static/swagger-ui-bundle.js",
+        swagger_css_url=f"{SETTINGS.root_path}/static/swagger-ui.css",
+    )
+
+
+@app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
+async def swagger_ui_redirect():
+    return get_swagger_ui_oauth2_redirect_html()
